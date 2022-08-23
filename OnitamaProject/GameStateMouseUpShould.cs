c00b2f3a2aa24bcd6e8834ef6f73fadd20b801b9
Omar Bonnet <omar.bonnet@gmail.com>
2022-08-23 16:21:49 -0400

    Added tests

---
+++ OnitamaProject/GameStateMouseUpShould.cs
@@ -0,0 +1,39 @@
+
+
+namespace OnitamaTests
+{
+    public class GameStateMouseUpShould
+    {
+        [Theory]
+        [InlineData(BoardItem.BlueCard1)]
+        [InlineData(BoardItem.BlueCard2)]
+        [InlineData(BoardItem.RedCard1)]
+        [InlineData(BoardItem.RedCard2)]
+        [InlineData(BoardItem.OffMenu)]
+        [InlineData(BoardItem.Menu)]
+        [InlineData(BoardItem.CloseMenu)]
+        [InlineData(BoardItem.BlueSurrender)]
+        [InlineData(BoardItem.RedSurrender)]
+        [InlineData(BoardItem.CloseGame)]
+        [InlineData(BoardItem.Square)]
+        [InlineData(BoardItem.TryAgain)]
+        [InlineData(BoardItem.Tutorial)]
+        [InlineData(BoardItem.NewGame)]
+
+        public void MouseUpWhenIsGameOverShouldDoNothing(BoardItem value)
+        {
+            var result = new GameState() { IsGameOver = true };
+            var candidate = result;
+            candidate.MouseUp(value, new System.Drawing.Point(0, 0));
+            Assert.Equal(result, candidate);
+        }
+
+        [Fact]
+        public void MouseUpOnCurrentTeamCardShouldActivate()
+        {
+            var candidate = new GameState() { TutorialStep = 0, CurrentTeam = Team.Blue };
+            candidate.MouseUp(BoardItem.BlueCard2, new System.Drawing.Point(0, 0));
+            Assert.True(candidate.ActiveCard == candidate.BlueCards[1]);
+        }
+    }
+}
