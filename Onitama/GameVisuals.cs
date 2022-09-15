c536ca63b094497af83342b2e2a9a368cf095e10
Omar Bonnet <omar.bonnet@gmail.com>
2022-09-15 09:00:11 -0400

    sync

--- Onitama/GameVisuals.cs
+++ Onitama/GameVisuals.cs
@@ -44,6 +44,8 @@ public class GameVisuals : DrawTools
 
     public bool IsGameOver { get; set; }
 
+    public List<RectangleF> DrawnInteractives { get; set; }
+
     private static RectangleF blueCard1Bounds = new(.425f, 1.88f, 1.8f, 2.25f);
     private static RectangleF blueCard2Bounds = new(.425f, 4.36f, 1.8f, 2.25f);
     private static RectangleF redCard1Bounds = new(7.7f, 1.88f, 1.8f, 2.25f);
@@ -51,6 +53,8 @@ public class GameVisuals : DrawTools
 
     public void DrawGame(Graphics g)
     {
+        DrawnInteractives.Clear();
+
         DrawBoard(g);
 
         DrawState(g);
@@ -71,11 +75,38 @@ public class GameVisuals : DrawTools
         }
     }
 
+    private void DrawBoard(Graphics g)
+    {
+        RectangleF board = new(0.05f, 0.05f, 9.9f, 6.9f);
+        RectangleF mat = new(0.15f, 1.65f, 9.7f, 5.2f);
+        RectangleF grid = new(2.5f, 1.75f, 5f, 5f);
+        RectangleF menuButton = new(0.1f, 0.1f, 0.6f, 0.225f);
+
+        DrawRoundedBox(g, board, new SolidBrush(Color.Green), BlackPen, 0);
+        g.FillRoundedRectangleF(MatBrush, mat, .1f);
+        DrawRoundedBox(g, grid, new SolidBrush(Color.Olive), BlackPen, 0);
+        DrawRoundedTextBox(g, "MENU", TutorialFont, menuButton, BlackPen, MoccasinBrush, BlackBrush, 0.05f, StringFormats.Center);
+        for (var y = 0; y < 5; y++)
+        {
+            for (var x = 0; x < 5; x++)
+            {
+                g.DrawRectangle(new Pen(Color.DarkOliveGreen, 0.03f), GridToView(x, y).X + 0.1f, GridToView(x, y).Y + 0.1f, 0.8f, 0.8f);
+            }
+        }
+
+        RectangleF templeBlue = new(GridToView(0, 2).X + 0.075f, GridToView(0, 2).Y + 0.075f, .85f, .85f);
+        RectangleF templeRed = new(GridToView(4, 2).X + 0.075f, GridToView(0, 2).Y + 0.075f, .85f, .85f);
+        g.DrawRoundedRectangleF(new Pen(Color.DarkBlue, 0.075f), templeBlue, 0.1f);
+        g.DrawRoundedRectangleF(new Pen(Color.DarkRed, 0.075f), templeRed, 0.1f);
+    }
+
     public void DrawState(Graphics g)
     {
         foreach (var piece in BlueStudents)
         {
-            g.DrawImage(Resources.blueStudentImg, new RectangleF(piece.X + GridOrigin.X + 0.1f, piece.Y + GridOrigin.Y + 0.1f, 0.8f, 0.8f));
+            var place = new RectangleF(piece.X + GridOrigin.X + 0.1f, piece.Y + GridOrigin.Y + 0.1f, 0.8f, 0.8f);
+            g.DrawImage(Resources.blueStudentImg, place);
+
         }
 
         foreach (var piece in RedStudents)
@@ -124,6 +155,7 @@ public class GameVisuals : DrawTools
                 _ => throw new Exception(),
             };
             DrawCard(g, card, location, isActive, team);
+            DrawnInteractives.Add(location);
         }
 
         RectangleF neutralCardBG = new(3f, 0.15f, 4f, 1.4f);
@@ -137,39 +169,6 @@ public class GameVisuals : DrawTools
         Brush teamBrush = CurrentTeam == Team.Blue ? DarkBlueBrush : DarkRedBrush;
 
         DrawRoundedTextBox(g, "YOUR TURN", Font, CurrentTeam == Team.Blue ? blueTurnBG : redTurnBG, BlackPen, teamBrush, MoccasinBrush, 0.02f, StringFormats.Center);
-    }
-
-    private void DrawBoard(Graphics g)
-    {
-        RectangleF board = new(0.05f, 0.05f, 9.9f, 6.9f);
-        RectangleF mat = new(0.15f, 1.65f, 9.7f, 5.2f);
-        RectangleF grid = new(2.5f, 1.75f, 5f, 5f);
-        RectangleF menuButton = new(0.1f, 0.1f, 0.6f, 0.225f);
-
-        DrawRoundedBox(g, board, new SolidBrush(Color.Green), BlackPen, 0);
-        g.FillRoundedRectangleF(MatBrush, mat, .1f);
-        DrawRoundedBox(g, grid, new SolidBrush(Color.Olive), BlackPen, 0);
-        DrawRoundedTextBox(g, "MENU", TutorialFont, menuButton, BlackPen, MoccasinBrush, BlackBrush, 0.05f, StringFormats.Center);
-        for (var y = 0; y < 5; y++)
-        {
-            for (var x = 0; x < 5; x++)
-            {
-                g.DrawRectangle(new Pen(Color.DarkOliveGreen, 0.03f), GridToView(x, y).X + 0.1f, GridToView(x, y).Y + 0.1f, 0.8f, 0.8f);
-            }
-        }
-
-        RectangleF templeBlue = new(GridToView(0, 2).X + 0.075f, GridToView(0, 2).Y + 0.075f, .85f, .85f);
-        RectangleF templeRed = new(GridToView(4, 2).X + 0.075f, GridToView(0, 2).Y + 0.075f, .85f, .85f);
-        g.DrawRoundedRectangleF(new Pen(Color.DarkBlue, 0.075f), templeBlue, 0.1f);
-        g.DrawRoundedRectangleF(new Pen(Color.DarkRed, 0.075f), templeRed, 0.1f);
-    }
-
-    private void DrawGameOver(Graphics g)
-    {
-        RectangleF gameOverBanner = new(3, 2.5f, 4, 2);
-        RectangleF playAgain = new(4.25f, 3.88f, 1.5f, 0.5f);
-        DrawRoundedTextBox(g, $"{CurrentTeam.ToString().ToUpper()} WINS!!!", new Font("Arial", 0.5f, FontStyle.Bold, GraphicsUnit.Pixel), gameOverBanner, new Pen(Color.Black, 0.05f), MoccasinBrush, BlackBrush, 0.5f, StringFormats.Center);
-        DrawRoundedTextBox(g, "Play Again", new Font("Arial", 0.225f, FontStyle.Bold, GraphicsUnit.Pixel), playAgain, new Pen(Color.Black, 0.02f), WhiteBrush, BlackBrush, 0.1f, StringFormats.Center);
     }
 
     private void DrawTutorial(Graphics g)
@@ -230,7 +229,16 @@ public class GameVisuals : DrawTools
         }
     }
 
-    private static void DrawMenu(Graphics g)
+    private void DrawGameOver(Graphics g)
+    {
+        RectangleF gameOverBanner = new(3, 2.5f, 4, 2);
+        RectangleF playAgain = new(4.25f, 3.88f, 1.5f, 0.5f);
+        DrawRoundedTextBox(g, $"{CurrentTeam.ToString().ToUpper()} WINS!!!", new Font("Arial", 0.5f, FontStyle.Bold, GraphicsUnit.Pixel), gameOverBanner, new Pen(Color.Black, 0.05f), MoccasinBrush, BlackBrush, 0.5f, StringFormats.Center);
+        DrawRoundedTextBox(g, "Play Again", new Font("Arial", 0.225f, FontStyle.Bold, GraphicsUnit.Pixel), playAgain, new Pen(Color.Black, 0.02f), WhiteBrush, BlackBrush, 0.1f, StringFormats.Center);
+        DrawnInteractives.Add(playAgain);
+    }
+
+    private void DrawMenu(Graphics g)
     {
         DrawRoundedTextBox(g, "MENU", TitleFont, new RectangleF(3.5f, 1f, 3f, 5f), BlackPen, MoccasinBrush, BlackBrush,  CornerRadius, StringFormats.CenterTop);
 
@@ -247,6 +255,7 @@ public class GameVisuals : DrawTools
         foreach (var button in menuButtons)
         {
             DrawMenuButton(g, button);
+            DrawnInteractives.Add(button.Bounds);
         }
     }
 
