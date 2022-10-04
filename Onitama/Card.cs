0feb4829871af02c29f3669b6feb38f8e787638e
Omar Bonnet <omar.bonnet@gmail.com>
2022-10-04 00:37:53 -0400

    Changed all cards to properties

--- Onitama/Card.cs
+++ Onitama/Card.cs
@@ -1,9 +1,13 @@
 using System.Collections.Immutable;
 
 namespace Onitama;
-
+// TODO finish test for team change on move
+// TODO add more tests
+// TODO Change Card names to be properties
+// TODO add pictures and build instructions to README
 public record Card
 {
+
     public ImmutableArray<Size> Moves { get; set; }
 
     public string Name { get; set; }
@@ -42,21 +46,39 @@ public record Card
         return card with { Moves = invertedMoves };
     }
 
+    public static Card Monkey { get; } = new("Monkey", ImmutableArray.Create(new Size(-1, -1), new Size(-1, 1), new Size(+1, 1), new Size(+1, -1)));
+    public static Card Rooster { get; } = new Card("Rooster", ImmutableArray.Create(new Size(-1, -1), new Size(0, -1), new Size(0, 1), new Size(1, 1)));
+    public static Card Horse { get; } = new("Horse", ImmutableArray.Create(new Size(-1, 0), new Size(0, -1), new Size(1, 0))),
+    public static Card Goose { get; } = new Card("Goose", ImmutableArray.Create(new Size(-1, 1), new Size(0, -1), new Size(0, 1), new Size(1, -1)));
+    public static Card Dragon { get; } = new("Dragon", ImmutableArray.Create(new Size(-1, -1), new Size(-1, 1), new Size(1, -2), new Size(1, 2)));
+    public static Card Rabbit { get; } = new Card("Rabbit", ImmutableArray.Create(new Size(-1, -1), new Size(0, 2), new Size(1, 1)));
+    public static Card Crab { get; } = new("Crab", ImmutableArray.Create(new Size(0, 2), new Size(0, -2), new Size(1, 0)));
+    public static Card Boar { get; } = new Card("Boar", ImmutableArray.Create(new Size(0, 1), new Size(0, -1), new Size(1, 0)));
+    public static Card Tiger { get; } = new("Tiger", ImmutableArray.Create(new Size(-1, 0), new Size(2, 0)));
+    public static Card Eel { get; } = new Card("Eel", ImmutableArray.Create(new Size(-1, -1), new Size(0, 1), new Size(1, -1)));
+    public static Card Mantis { get; } = new Card("Mantis", ImmutableArray.Create(new Size(-1, 0), new Size(1, -1), new Size(1, 1)));
+    public static Card Ox { get; } = new Card("Ox", ImmutableArray.Create(new Size(-1, 0), new Size(0, 1), new Size(1, 0)));
+    public static Card Frog { get; } = new Card("Frog", ImmutableArray.Create(new Size(-1, 1), new Size(0, -2), new Size(1, -1)));
+    public static Card Elephant { get; } = new Card("Elephant", ImmutableArray.Create(new Size(0, -1), new Size(0, 1), new Size(1, -1), new Size(1, 1)));
+    public static Card Cobra { get; } = new Card("Cobra", ImmutableArray.Create(new Size(-1, 1), new Size(0, -1), new Size(1, 1)));
+    public static Card Crane { get; } = new Card("Crane", ImmutableArray.Create(new Size(-1, -1), new Size(-1, 1), new Size(1, 0))));
+
     public static ImmutableArray<Card> Deck { get; } = ImmutableArray.Create(
-    new Card("Monkey", ImmutableArray.Create(new Size(-1, -1), new Size(-1, 1), new Size(+1, 1), new Size(+1, -1))),
-    new Card("Rooster", ImmutableArray.Create(new Size(-1, -1), new Size(0, -1), new Size(0, 1), new Size(1, 1))),
-    new Card("Horse", ImmutableArray.Create(new Size(-1, 0), new Size(0, -1), new Size(1, 0))),
-    new Card("Goose", ImmutableArray.Create(new Size(-1, 1), new Size(0, -1), new Size(0, 1), new Size(1, -1))),
-    new Card("Dragon", ImmutableArray.Create(new Size(-1, -1), new Size(-1, 1), new Size(1, -2), new Size(1, 2))),
-    new Card("Rabbit", ImmutableArray.Create(new Size(-1, -1), new Size(0, 2), new Size(1, 1))),
-    new Card("Crab", ImmutableArray.Create(new Size(0, 2), new Size(0, -2), new Size(1, 0))),
-    new Card("Boar", ImmutableArray.Create(new Size(0, 1), new Size(0, -1), new Size(1, 0))),
-    new Card("Tiger", ImmutableArray.Create(new Size(-1, 0), new Size(2, 0))),
-    new Card("Eel", ImmutableArray.Create(new Size(-1, -1), new Size(0, 1), new Size(1, -1))),
-    new Card("Mantis", ImmutableArray.Create(new Size(-1, 0), new Size(1, -1), new Size(1, 1))),
-    new Card("Ox", ImmutableArray.Create(new Size(-1, 0), new Size(0, 1), new Size(1, 0))),
-    new Card("Frog", ImmutableArray.Create(new Size(-1, 1), new Size(0, -2), new Size(1, -1))),
-    new Card("Elephant", ImmutableArray.Create(new Size(0, -1), new Size(0, 1), new Size(1, -1), new Size(1, 1))),
-    new Card("Cobra", ImmutableArray.Create(new Size(-1, 1), new Size(0, -1), new Size(1, 1))),
-    new Card("Crane", ImmutableArray.Create(new Size(-1, -1), new Size(-1, 1), new Size(1, 0))));
+    Monkey,
+    Rooster,
+    Horse,
+    Goose,
+    Dragon,
+    Rabbit,
+    Crab,
+    Boar,
+    Tiger,
+    Eel,
+    Mantis,
+    Ox,
+    Frog,
+    Elephant,
+    Cobra,
+    Crane
+    );
 }
